f8c1f2353bef42883de5ec5b51de7c68668094c3
juno <juno@krasava.fi>
2015-02-12 03:46:56 +0200

    dev

--- usefull.bash
+++ usefull.bash
@@ -1,7 +1,8 @@
 GOPATH=$GOPATH:/home/juno/git/imagehoster go test -v
 
- find . -type f |grep '^./0' |awk '{print "mv "$0" dest"$0  }'
- 
+
+
+ find . -type f |grep '^./0' |awk '{print "mv "$0" dest"$0  }' 
  find . -type f |grep '^./0' |awk '{print "mkdir -p `dirname "$0"`; mv "$0" dest"$0  }'
  
  :1,96s/dest.\/0/.\//
